9d2b6149baeb407b4d87b0b1402d79a08d19ccfe
Ari Nurcahya <nurcahyaari@gmail.com>
2020-11-01 21:41:55 +0700

    update webpack configure for production

--- config/webpack/webpack.prod.js
+++ config/webpack/webpack.prod.js
@@ -3,6 +3,19 @@ const { merge } = require('webpack-merge');
 
 const config = {
     mode: 'production',
+
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    chunks: 'initial',
+                    name: 'vendor',
+                    test: module => /node_modules/.test(module.resource),
+                    enforce: true,
+                }
+            }
+        }
+    }
 };
 
 module.exports = merge(
